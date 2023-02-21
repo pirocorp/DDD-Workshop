71aab21d4d15ccc8b4887b77abbcbcb656811c79
Zdravko Zdravkov <zdr0@yahoo.com>
2023-02-21 11:21:15 +0100

    Add Bootstrapping API project

---
+++ src/CarRentalSystem.Startup/Program.cs
@@ -0,0 +1,52 @@
+namespace CarRentalSystem.Startup;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        ConfigureConfiguration(builder.Configuration);
+        ConfigureServices(builder.Services);
+
+        var app = builder.Build();
+
+        ConfigureMiddleware(app);
+        ConfigureEndpoints(app);
+
+        app.Run();
+    }
+
+    private static void ConfigureConfiguration(IConfiguration configuration)
+    { }
+
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    private static void ConfigureMiddleware(WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+    }
+
+    private static void ConfigureEndpoints(WebApplication app)
+    {
+        app.MapControllers();
+    }
+}
