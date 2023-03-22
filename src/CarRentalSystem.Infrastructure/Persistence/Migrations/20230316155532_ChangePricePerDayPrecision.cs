7007bb4ce1d05e7381c2efea050837a7d7021400
Zdravko Zdravkov <zdr0@yahoo.com>
2023-03-22 15:37:58 +0100

    Decimal precision change - database migration

---
+++ src/CarRentalSystem.Infrastructure/Persistence/Migrations/20230316155532_ChangePricePerDayPrecision.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarRentalSystem.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangePricePerDayPrecision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PricePerDay",
+                table: "CarAds",
+                type: "decimal(38,18)",
+                precision: 38,
+                scale: 18,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PricePerDay",
+                table: "CarAds",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(38,18)",
+                oldPrecision: 38,
+                oldScale: 18);
+        }
+    }
+}
